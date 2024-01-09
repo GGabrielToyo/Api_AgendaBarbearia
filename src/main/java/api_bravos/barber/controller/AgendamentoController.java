61f9068be137a879434d8786ee37ee39488c38b5
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2024-01-09 18:07:55 -0300

    Atualização do crontoller de agenda com um método que recebe uma data, a converte para formato LocalDateTime, faz a query no banco para buscar os agendamentos naquele dia e retorna pro front.

--- src/main/java/api_bravos/barber/controller/AgendamentoController.java
+++ src/main/java/api_bravos/barber/controller/AgendamentoController.java
@@ -10,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @RequestMapping("/agendamentos")
 public class AgendamentoController {
@@ -31,9 +34,22 @@ public class AgendamentoController {
     @GetMapping
     public ResponseEntity<Page<DadosDetalhamentoAgendamento>> listar(Pageable paginacao) {
         var lista = repository.findAll(paginacao).map(DadosDetalhamentoAgendamento::new);
-
         return ResponseEntity.ok(lista);
     }
+
+    /*
+    TODO Data chegando do front e sendo tratada para formato correto.
+        ->Fazer a query no banco buscando pela data para saber os horários
+          indisponíveis
+    */
+    @GetMapping("/{data}")
+    public void listarHorariosNaData(@PathVariable String data){
+        String dataSemFracaoSegundo = data.substring(0, data.length() - 5);
+        LocalDateTime localDateTime = LocalDateTime.parse(dataSemFracaoSegundo, DateTimeFormatter.ISO_DATE_TIME);
+
+        System.out.println(localDateTime);
+    }
+
 
     @DeleteMapping
     @Transactional
